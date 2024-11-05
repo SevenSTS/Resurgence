b0f8985c19fedd77d7d44548d2bce41a87ec5f91
SevenSTS <143455202+SevenSTS@users.noreply.github.com>
2024-11-05 22:09:00 +0100

    Update index.js

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Nous ne vous demanderons jamais votre mot de passe", "author": "Seven" },
+            { "message": "Salut je suis du code.", "author": "Seven" },
+            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Seven" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
